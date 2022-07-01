d92771d837317d1ff72ad4dbe52f49f578c22f59
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-06-30 19:28:45 -0500

    authentication

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,7 +1,4 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import "firebase/compat/storage";
+import firebase from "firebase";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
